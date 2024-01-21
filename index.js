cc43070afcf6299ffd442334ee038df6307782c2
Dheeraj-2205 <djsays124@gmail.com>
2024-01-21 19:23:41 +0530

    adding the data

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get("/dheeraj", (req,res) =>{
     res.send("<h1>hii dheeraj your server is working perfectly<h1/>")
 });
 
+app.get("/github", (req,res) =>{
+    res.send("<h1>New Backend Series<h1/>")
+});
+
+app.get("/data", (req,res)=>{
+    res.json({
+        name : "dheeraj",
+        age : 25
+    })
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log(`server is listening`);
 })
